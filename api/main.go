d5c9374dbde8cfac74fb67557a095b70c0d8fd33
wojtek <wojlas93@gmail.com>
2026-03-17 21:07:55 +0100

    error handler with nil

--- api/main.go
+++ api/main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+)
+
+type Source string
+type Category string
+
+const (
+	CatSport  Category = "Sport"
+	CatMoto   Category = "Moto"
+	CatGossip Category = "Gossip"
+	CatInfo   Category = "Info"
+)
+
+const (
+	Onet   Source = "Onet.pl"
+	WP     Source = "Wp.pl"
+	O2     Source = "o2.pl"
+	Gazeta Source = "gazeta.pl"
 )
 
 type Article struct {
 	Title       string
-	Source      string
-	Category    string
+	Source      Source
+	Category    Category
 	Link        string
 	Description string
 }
@@ -16,36 +34,49 @@ func main() {
 	articles := []Article{
 		{
 			Title:       "Test Onet",
-			Source:      "Onet.pl",
-			Category:    "Sport",
+			Source:      "Onet.pla",
+			Category:    CatSport,
 			Link:        "www.onet.pl",
 			Description: "test news onet",
 		},
 		{
 			Title:       "Test Wp",
 			Source:      "Wp.pl",
-			Category:    "Info",
+			Category:    CatInfo,
 			Link:        "www.wp.pl",
 			Description: "test news wp",
 		},
 		{
 			Title:       "Test o2",
 			Source:      "o2.pl",
-			Category:    "Gossip",
+			Category:    CatGossip,
 			Link:        "www.02.pl",
 			Description: "test news o2",
 		},
 		{
 			Title:       "Test gazeta",
 			Source:      "gazeta.pl",
-			Category:    "Moto",
+			Category:    CatMoto,
 			Link:        "www.gazeta.pl",
 			Description: "test news gazeta",
 		},
 	}
 
-	for _, s := range articles {
-		fmt.Println(s)
+	found, err := findArticleByTitle(articles, "Test Onet")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Found:", found.Title)
+	}
+}
+
+func findArticleByTitle(articles []Article, title string) (Article, error) {
+	for _, a := range articles {
+		if a.Title == title {
+			return a, nil
+		}
 	}
 
+	return Article{}, errors.New("no article found")
 }
